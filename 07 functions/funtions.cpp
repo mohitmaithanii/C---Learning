abc1310bda59fbdfa092aa987be94f36252f4519
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-05-27 20:37:40 +0530

    functions in cpp

---
+++ 07 functions/funtions.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std;
+
+// Functions :
+// A function is a block of code that performs a specific task.
+
+// declaring the greet function
+void greet() {
+   cout << "Hello there!";
+}
+
+// The main function is the entry point of the program.
+int main() {
+
+   // calling the greet function
+   greet();
+
+   return 0;
+}
