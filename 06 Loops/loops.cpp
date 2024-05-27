718cf61546541c1c1e62a4b3517f0b80382e3900
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-05-27 20:28:22 +0530

    "Added switch-case statement to handle basic arithmetic operations"

--- 06 Loops/loops.cpp
+++ 06 Loops/loops.cpp
@@ -43,6 +43,34 @@ int main() {
       cout << i << endl;
    }
 
+   // switch..case :
+   // The switch statement allows us to execute a block of code among many alternatives.
+
+   char oper;
+   float num1, num2;
+   cout << "Enter an operator (+, -, *, /): ";
+   cin >> oper;
+   cout << "Enter two numbers: " << endl;
+   cin >> num1 >> num2;
+
+   switch (oper) {
+   case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+   case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+   case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+   case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+   default:
+      // operator is doesn't match any case constant (+, -, *, /)
+      cout << "Error! The operator is not correct";
+      break;
+   }
 
 
    return 0;
